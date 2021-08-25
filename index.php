e51ac4d9301a930b4085752bc32c193b87b0b2ee
ricardoaugustob <ricardoaugustob@outlook.com>
2021-08-25 14:22:30 -0300

    Alterando

--- index.php
+++ index.php
@@ -4,13 +4,17 @@ $categorias[] = "Infantil";
 $categorias[] = "Adolecente";
 $categorias[] = "Adulto";
 
-echo "Essas são todas as categoraias do meu array";
-for($i=0; $i<count($categorias); $i++ ){
-    echo "<br>".$categorias[$i];
+//echo "Essas são todas as categoraias do meu array";
+//for($i=0; $i<count($categorias); $i++ ){
+//    echo "<br>".$categorias[$i];
 
 
-}
+//}
 
 
+$primeiro_nome= $_POST["fname"];
+$segundo_nome= $_POST["lname"];
+
+echo "Olá ".$primeiro_nome ." ".$segundo_nome;
 
 ?>
