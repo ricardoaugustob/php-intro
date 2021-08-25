41af324248099ac1ec342ad0b2717c3b4f041bf4
ricardoaugustob <ricardoaugustob@outlook.com>
2021-08-25 08:43:16 -0300

    modificando

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $categorias[] = "Infantil";
 $categorias[] = "Adolecente";
 $categorias[] = "Adulto";
 
-echo "Essas são todas as categoraias do meu array\n";
+echo "Essas são todas as categoraias do meu array";
 for($i=0; $i<count($categorias); $i++ ){
     echo "<br>".$categorias[$i];
 
